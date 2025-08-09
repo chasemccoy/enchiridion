7910f3b31c3b4cdc6c922c360355d6c7300a43e6
Chase McCoy <chasem000@gmail.com>
2025-08-08 19:19:56 -0500

    ~

--- app/composables/useRecords.ts
+++ app/composables/useRecords.ts
@@ -2,16 +2,17 @@ import useApiClient, { ApiEndpoints } from '@app/composables/useApiClient';
 import { useQuery } from '@tanstack/vue-query';
 import type { ListRecordsAPIResponse } from '@db/queries/records';
 import type { ListRecordsInput } from '@shared/types/api';
+import { toValue, type MaybeRef } from 'vue';
 
-export default function useRecords(options?: ListRecordsInput) {
-	const { fetch } = useApiClient();
+export default function useRecords(options?: MaybeRef<ListRecordsInput>) {
+  const { fetch } = useApiClient();
 
-	return useQuery({
-		queryKey: ['list-records', options],
-		queryFn: () =>
-			fetch<ListRecordsAPIResponse>(ApiEndpoints.records, {
-				method: 'POST',
-				body: JSON.stringify(options),
-			}),
-	});
+  return useQuery({
+    queryKey: ['list-records', options],
+    queryFn: () =>
+      fetch<ListRecordsAPIResponse>(ApiEndpoints.records, {
+        method: 'POST',
+        body: JSON.stringify(toValue(options)),
+      }),
+  });
 }
